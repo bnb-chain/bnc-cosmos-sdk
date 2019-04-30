d1b9583d07b9ebc9f26680f5afd8934daa274bbe
yutianwu <wzxingbupt@gmail.com>
2019-04-30 14:19:49 +0800

    fix command

--- x/gov/client/cli/commands.go
+++ x/gov/client/cli/commands.go
@@ -22,7 +22,7 @@ func AddCommands(cmd *cobra.Command, cdc *codec.Codec) {
 			GetCmdDeposit(cdc),
 			GetCmdSubmitProposal(cdc),
 			GetCmdSubmitListProposal(cdc),
-			GetCmdSubmitListProposal(cdc),
+			GetCmdSubmitDelistProposal(cdc),
 			GetCmdVote(cdc),
 		)...,
 	)
