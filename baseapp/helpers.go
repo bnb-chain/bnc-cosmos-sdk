149b2fc0a2ad91cd3c7b54a73d5cf93a68023175
rickyyangz <yangzhongzhong@ruiquewenhua.com>
2019-05-09 11:13:32 +0800

    upgrade tendermint to 0315

--- baseapp/helpers.go
+++ baseapp/helpers.go
@@ -2,8 +2,6 @@ package baseapp
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/tendermint/tendermint/abci/server"
-	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/crypto/tmhash"
 	cmn "github.com/tendermint/tendermint/libs/common"
 )
@@ -24,29 +22,4 @@ func (app *BaseApp) Simulate(txBytes []byte, tx sdk.Tx) (result sdk.Result) {
 func (app *BaseApp) Deliver(tx sdk.Tx) (result sdk.Result) {
 	txHash := cmn.HexBytes(tmhash.Sum(nil)).String()
 	return app.RunTx(sdk.RunTxModeDeliver, nil, tx, txHash)
-}
-
-// RunForever - BasecoinApp execution and cleanup
-func RunForever(app abci.Application) {
-
-	// Start the ABCI server
-	srv, err := server.NewServer("0.0.0.0:26658", "socket", app)
-	if err != nil {
-		cmn.Exit(err.Error())
-		return
-	}
-	err = srv.Start()
-	if err != nil {
-		cmn.Exit(err.Error())
-		return
-	}
-
-	// Wait forever
-	cmn.TrapSignal(func() {
-		// Cleanup
-		err := srv.Stop()
-		if err != nil {
-			cmn.Exit(err.Error())
-		}
-	})
 }
