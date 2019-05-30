0a3d35a9d4a19a5ec5bae232bd3e552e7a72b791
yutianwu <wzxingbupt@gmail.com>
2019-05-30 17:25:57 +0800

    add unit tests

--- types/upgrade_test.go
+++ types/upgrade_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 const UpgradeTest = "upgradeTest"
+const StoreKeyNameTest = "storeKeyTest"
 
 func TestUpgrade(t *testing.T) {
 	UpgradeMgr = NewUpgradeManager(UpgradeConfig{})
@@ -62,5 +63,59 @@ func TestUpgrade(t *testing.T) {
 		UpgradeMgr.SetHeight(tc.height)
 		require.Equal(t, tc.upgradeResult, IsUpgrade(UpgradeTest))
 		require.Equal(t, tc.heightResult, IsUpgradeHeight(UpgradeTest))
+	}
+}
+
+func TestStoreKey(t *testing.T) {
+	UpgradeMgr = NewUpgradeManager(UpgradeConfig{})
+
+	type testCase struct {
+		config       UpgradeConfig
+		height       int64
+		shouldCommit bool
+	}
+
+	testCases := []testCase{
+		{
+			config: UpgradeConfig{
+				HeightMap: map[string]int64{},
+			},
+			height:       10000,
+			shouldCommit: true,
+		},
+		{
+			config: UpgradeConfig{
+				HeightMap: map[string]int64{
+					UpgradeTest: 545000,
+				},
+			},
+			height:       10000,
+			shouldCommit: false,
+		}, {
+			config: UpgradeConfig{
+				HeightMap: map[string]int64{
+					UpgradeTest: 545000,
+				},
+			},
+			height:       545000,
+			shouldCommit: true,
+		}, {
+			config: UpgradeConfig{
+				HeightMap: map[string]int64{
+					UpgradeTest: 545000,
+				},
+			},
+			height:       545001,
+			shouldCommit: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		UpgradeMgr.AddConfig(tc.config)
+		if UpgradeMgr.GetUpgradeHeight(UpgradeTest) != 0 {
+			UpgradeMgr.RegisterStoreKey(UpgradeTest, StoreKeyNameTest)
+		}
+		UpgradeMgr.SetHeight(tc.height)
+		require.Equal(t, tc.shouldCommit, ShouldCommitStore(StoreKeyNameTest))
 	}
 }
