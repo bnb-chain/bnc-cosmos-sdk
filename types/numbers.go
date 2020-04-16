1fc400965fbebb1a3c9535f7a175a14714dbb355
fletcher.fan <fletcher.fan@binance.com>
2020-04-16 13:41:15 +0800

    remove unused function

--- types/numbers.go
+++ types/numbers.go
@@ -17,18 +17,6 @@ func Mul64(a, b int64) (int64, bool) {
 	return c, false
 }
 
-func MulBigInt64(a, b *big.Int) *big.Int {
-	var bi big.Int
-	bi.Mul(a, b)
-	return &bi
-}
-
-func DivBigInt64(x, y *big.Int) *big.Int {
-	var bi big.Int
-	bi.Div(x, y)
-	return &bi
-}
-
 func MulDivDec(a, b, c Dec) (Dec, bool) {
 	r, ok := Mul64(a.RawInt(), b.RawInt())
 	if !ok {
@@ -40,43 +28,4 @@ func MulDivDec(a, b, c Dec) (Dec, bool) {
 		return NewDec(bi.Int64()), true
 	}
 	return NewDec(r / c.RawInt()), true
-}
-
-func MulDivDecWithExtraDecimal(a, b, c Dec, extraDecimalPlace int) (afterRoundDown int64, extraDecimalValue int) {
-	extra := int64(Pow(10, extraDecimalPlace))
-	product, ok := Mul64(a.RawInt(), b.RawInt())
-	if !ok { // int64 exceed
-		return mulDivBig64WithExtraDecimal(big.NewInt(a.RawInt()), big.NewInt(b.RawInt()), big.NewInt(c.RawInt()), big.NewInt(extra))
-	} else {
-		if product, ok = Mul64(product, extra); !ok {
-			return mulDivBig64WithExtraDecimal(big.NewInt(a.RawInt()), big.NewInt(b.RawInt()), big.NewInt(c.RawInt()), big.NewInt(extra))
-		}
-		resultOfAddDecimalPlace := product / c.RawInt()
-		afterRoundDown = resultOfAddDecimalPlace / extra
-		extraDecimalValue = int(resultOfAddDecimalPlace % extra)
-		return afterRoundDown, extraDecimalValue
-	}
-}
-
-func mulDivBig64WithExtraDecimal(a, b, c, extra *big.Int) (afterRoundDown int64, extraDecimalValue int) {
-	product := MulBigInt64(MulBigInt64(a, b), extra)
-	result := DivBigInt64(product, c)
-
-	expectedDecimalValueBig := &big.Int{}
-	afterRoundDownBig, expectedDecimalValueBig := result.QuoRem(result, extra, expectedDecimalValueBig)
-	afterRoundDown = afterRoundDownBig.Int64()
-	extraDecimalValue = int(expectedDecimalValueBig.Int64())
-	return afterRoundDown, extraDecimalValue
-}
-
-func Pow(x, n int) int {
-	ret := 1
-	for n != 0 {
-		if n%2 != 0 {
-			ret = ret * x
-		}
-		n /= 2
-		x = x * x
-	}
-	return ret
 }
--- types/numbers_test.go
+++ types/numbers_test.go
@@ -20,36 +20,3 @@ func TestMulDivDec(t *testing.T) {
 	require.True(t, ok)
 	require.EqualValues(t, 4000000000000000000, r.RawInt())
 }
-
-func TestMulDivDecWithExtraDecimal(t *testing.T) {
-	// 8/7 = 1.14285714,285714...
-	a := NewDec(2e8)
-	b := NewDec(4e8)
-	c := NewDec(7e8)
-	afterRoundDown, extraDecimalValue := MulDivDecWithExtraDecimal(a, b, c, 1)
-	require.EqualValues(t, 114285714, afterRoundDown)
-	require.EqualValues(t, 2, extraDecimalValue)
-	afterRoundDown, extraDecimalValue = MulDivDecWithExtraDecimal(a, b, c, 6)
-	require.EqualValues(t, 114285714, afterRoundDown)
-	require.EqualValues(t, 285714, extraDecimalValue)
-	// 800/7 = 114.28571428,5714...
-	a = NewDec(20e8)
-	b = NewDec(40e8)
-	c = NewDec(7e8)
-	afterRoundDown, extraDecimalValue = MulDivDecWithExtraDecimal(a, b, c, 1)
-	require.EqualValues(t, 11428571428, afterRoundDown)
-	require.EqualValues(t, 5, extraDecimalValue)
-	afterRoundDown, extraDecimalValue = MulDivDecWithExtraDecimal(a, b, c, 4)
-	require.EqualValues(t, 11428571428, afterRoundDown)
-	require.EqualValues(t, 5714, extraDecimalValue)
-	// 8000/7 = 1142.85714285,714...
-	a = NewDec(200e8)
-	b = NewDec(40e8)
-	c = NewDec(7e8)
-	afterRoundDown, extraDecimalValue = MulDivDecWithExtraDecimal(a, b, c, 1)
-	require.EqualValues(t, 114285714285, afterRoundDown)
-	require.EqualValues(t, 7, extraDecimalValue)
-	afterRoundDown, extraDecimalValue = MulDivDecWithExtraDecimal(a, b, c, 3)
-	require.EqualValues(t, 114285714285, afterRoundDown)
-	require.EqualValues(t, 714, extraDecimalValue)
-}
