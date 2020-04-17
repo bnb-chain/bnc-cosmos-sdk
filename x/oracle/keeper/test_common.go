0bbfcbe5f57fa22311f3a10acfb8a1e13b0bfbcc
yutianwu <wzxingbupt@gmail.com>
2020-04-17 18:54:24 +0800

    delete unused code

--- x/oracle/keeper/test_common.go
+++ x/oracle/keeper/test_common.go
@@ -1,9 +1,6 @@
 package keeper
 
 import (
-	"bytes"
-	"log"
-	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -62,61 +59,4 @@ func getInitChainer(mapp *mock.App, stakeKeeper stake.Keeper) sdk.InitChainer {
 			Validators: validators,
 		}
 	}
-}
-
-// TODO: Remove once address interface has been implemented (ref: #2186)
-func SortValAddresses(addrs []sdk.ValAddress) {
-	var byteAddrs [][]byte
-	for _, addr := range addrs {
-		byteAddrs = append(byteAddrs, addr.Bytes())
-	}
-
-	SortByteArrays(byteAddrs)
-
-	for i, byteAddr := range byteAddrs {
-		addrs[i] = byteAddr
-	}
-}
-
-// Sorts Addresses
-func SortAddresses(addrs []sdk.AccAddress) {
-	var byteAddrs [][]byte
-	for _, addr := range addrs {
-		byteAddrs = append(byteAddrs, addr.Bytes())
-	}
-	SortByteArrays(byteAddrs)
-	for i, byteAddr := range byteAddrs {
-		addrs[i] = byteAddr
-	}
-}
-
-// implement `Interface` in sort package.
-type sortByteArrays [][]byte
-
-func (b sortByteArrays) Len() int {
-	return len(b)
-}
-
-func (b sortByteArrays) Less(i, j int) bool {
-	// bytes package already implements Comparable for []byte.
-	switch bytes.Compare(b[i], b[j]) {
-	case -1:
-		return true
-	case 0, 1:
-		return false
-	default:
-		log.Panic("not fail-able with `bytes.Comparable` bounded [-1, 1].")
-		return false
-	}
-}
-
-func (b sortByteArrays) Swap(i, j int) {
-	b[j], b[i] = b[i], b[j]
-}
-
-// Public
-func SortByteArrays(src [][]byte) [][]byte {
-	sorted := sortByteArrays(src)
-	sort.Sort(sorted)
-	return sorted
 }
