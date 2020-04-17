85d0d3c75ea519343a36770c75d36251d4c3a91d
fletcher.fan <fletcher.fan@binance.com>
2020-04-17 15:28:35 +0800

    refactor reward allocation

--- types/numbers_test.go
+++ types/numbers_test.go
@@ -39,3 +39,36 @@ func TestMulQuoDec(t *testing.T) {
 	require.EqualError(t,err,ErrZeroDividend)
 
 }
+
+func TestMulDivDecWithExtraDecimal(t *testing.T) {
+	// 8/7 = 1.14285714,285714...
+	a := NewDec(2e8)
+	b := NewDec(4e8)
+	c := NewDec(7e8)
+	afterRoundDown, extraDecimalValue := MulQuoDecWithExtraDecimal(a, b, c, 1)
+	require.EqualValues(t, 114285714, afterRoundDown)
+	require.EqualValues(t, 2, extraDecimalValue)
+	afterRoundDown, extraDecimalValue = MulQuoDecWithExtraDecimal(a, b, c, 6)
+	require.EqualValues(t, 114285714, afterRoundDown)
+	require.EqualValues(t, 285714, extraDecimalValue)
+	// 800/7 = 114.28571428,5714...
+	a = NewDec(20e8)
+	b = NewDec(40e8)
+	c = NewDec(7e8)
+	afterRoundDown, extraDecimalValue = MulQuoDecWithExtraDecimal(a, b, c, 1)
+	require.EqualValues(t, 11428571428, afterRoundDown)
+	require.EqualValues(t, 5, extraDecimalValue)
+	afterRoundDown, extraDecimalValue = MulQuoDecWithExtraDecimal(a, b, c, 4)
+	require.EqualValues(t, 11428571428, afterRoundDown)
+	require.EqualValues(t, 5714, extraDecimalValue)
+	// 8000/7 = 1142.85714285,714...
+	a = NewDec(200e8)
+	b = NewDec(40e8)
+	c = NewDec(7e8)
+	afterRoundDown, extraDecimalValue = MulQuoDecWithExtraDecimal(a, b, c, 1)
+	require.EqualValues(t, 114285714285, afterRoundDown)
+	require.EqualValues(t, 7, extraDecimalValue)
+	afterRoundDown, extraDecimalValue = MulQuoDecWithExtraDecimal(a, b, c, 3)
+	require.EqualValues(t, 114285714285, afterRoundDown)
+	require.EqualValues(t, 714, extraDecimalValue)
+}
