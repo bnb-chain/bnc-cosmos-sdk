802c570b415318bcabedfecc2442184eb0ee7905
fudongbai <296179868@qq.com>
2020-05-29 11:17:35 +0800

    fix comments

--- x/gov/handler_sidechain.go
+++ x/gov/handler_sidechain.go
@@ -6,10 +6,9 @@ import (
 )
 
 func handleMsgSideChainSubmitProposal(ctx sdk.Context, keeper Keeper, msg MsgSideChainSubmitProposal) sdk.Result {
-	if scCtx, err := keeper.ScKeeper.PrepareCtxForSideChain(ctx, msg.SideChainId); err != nil {
+	ctx, err := keeper.ScKeeper.PrepareCtxForSideChain(ctx, msg.SideChainId)
+	if err != nil {
 		return ErrInvalidSideChainId(keeper.codespace, msg.SideChainId).Result()
-	} else {
-		ctx = scCtx
 	}
 
 	result := handleMsgSubmitProposal(ctx, keeper, msg.MsgSubmitProposal)
@@ -20,11 +19,11 @@ func handleMsgSideChainSubmitProposal(ctx sdk.Context, keeper Keeper, msg MsgSid
 }
 
 func handleMsgSideChainDeposit(ctx sdk.Context, keeper Keeper, msg MsgSideChainDeposit) sdk.Result {
-	if scCtx, err := keeper.ScKeeper.PrepareCtxForSideChain(ctx, msg.SideChainId); err != nil {
+	ctx, err := keeper.ScKeeper.PrepareCtxForSideChain(ctx, msg.SideChainId)
+	if err != nil {
 		return ErrInvalidSideChainId(keeper.codespace, msg.SideChainId).Result()
-	} else {
-		ctx = scCtx
 	}
+
 	result := handleMsgDeposit(ctx, keeper, msg.MsgDeposit)
 	if result.IsOK() {
 		result.Tags = result.Tags.AppendTag(events.SideChainID, []byte(msg.SideChainId))
@@ -33,10 +32,9 @@ func handleMsgSideChainDeposit(ctx sdk.Context, keeper Keeper, msg MsgSideChainD
 }
 
 func handleMsgSideChainVote(ctx sdk.Context, keeper Keeper, msg MsgSideChainVote) sdk.Result {
-	if scCtx, err := keeper.ScKeeper.PrepareCtxForSideChain(ctx, msg.SideChainId); err != nil {
+	ctx, err := keeper.ScKeeper.PrepareCtxForSideChain(ctx, msg.SideChainId)
+	if err != nil {
 		return ErrInvalidSideChainId(keeper.codespace, msg.SideChainId).Result()
-	} else {
-		ctx = scCtx
 	}
 	result := handleMsgVote(ctx, keeper, msg.MsgVote)
 	if result.IsOK() {
