a34a83a8cf76926c7a62820b3e2ad9c4e0449e7d
Owen Hu <owen.h@nodereal.io>
2022-09-02 03:44:31 +0800

    chore: edit script

--- scripts/importssort.sh
+++ scripts/importssort.sh
@@ -1,9 +1,9 @@
 go get -u github.com/AanZee/goimportssort
 
+# only change files different from develop branch
 for entry in `git diff --name-only develop . | grep '\.go$'`; do
     echo $entry
     if grep -q "DO NOT EDIT" "$entry"; then
-      echo "xxxxxxxx=================================="
       continue
     fi
     goimportssort -w -local github.com/bnb-chain/ $entry
