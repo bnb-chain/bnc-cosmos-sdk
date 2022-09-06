82deed0b8eae9c8ce184028ce5052e04726f1aae
Owen Hu <owen.h@nodereal.io>
2022-09-06 10:40:56 +0800

    fix MsgEditSideChainValidator issue

--- x/stake/types/msg_sidechain.go
+++ x/stake/types/msg_sidechain.go
@@ -187,6 +187,9 @@ func (msg MsgEditSideChainValidator) ValidateBasic() sdk.Error {
 	}
 
 	if len(msg.SideConsAddr) != 0 {
+		if sdk.IsUpgrade(sdk.BEP159) {
+			return sdk.NewError(DefaultCodespace, CodeInvalidInput, "side consensus address cannot be updated before BEP159")
+		}
 		if err := checkSideChainAddr("SideConsAddr", msg.SideConsAddr); err != nil {
 			return err
 		}
