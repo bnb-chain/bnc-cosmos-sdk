38d46e284b8d8b54dc607ac107d32630284185c0
yutianwu <wzxingbupt@gmail.com>
2023-03-13 17:32:40 +0800

    sec: implement security enhancements

--- store/statesync_helper.go
+++ store/statesync_helper.go
@@ -200,6 +200,9 @@ func (helper *StateSyncHelper) WriteRecoveryChunk(hash abci.SHA256Sum, chunk *ab
 					return err
 				}
 			}
+			if numOfNodes == 0 {
+				return fmt.Errorf("length of nodes is 0")
+			}
 			nodeIdx := chunk.StartIdx + int64(numOfNodes-1)
 			helper.incompleteChunks[nodeIdx] = append(helper.incompleteChunks[nodeIdx],
 				incompleteChunkItem{
@@ -276,6 +279,10 @@ func (helper *StateSyncHelper) saveIncompleteChunks() error {
 		helper.logger.Debug("processing incomplete node", "nodeIdx", nodeIdx)
 		// sort and check chunkItems are valid
 		sort.Sort(&chunkItemSorter{chunkItems})
+
+		if len(chunkItems) == 0 {
+			return fmt.Errorf("length of chunks is 0")
+		}
 
 		expectedNodeParts := chunkItems[len(chunkItems)-1].chunkIdx - chunkItems[0].chunkIdx + 1
 		if expectedNodeParts != len(chunkItems) {
